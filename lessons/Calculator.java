1711ef4c4c2bec49874715a467808095afe71d4a
412Room <alex55reg@mail.ru>
2017-07-23 19:55:57 +0300

    user input

--- lessons/Calculator.java
+++ lessons/Calculator.java
@@ -1,40 +1,58 @@
-﻿public class Calculator {
+﻿/**
+ * Класс для выполнения арифметических операций
+ * @autor Плеханов
+ * @version 1.1
+ */
+public class Calculator {
 	/**
-		Результат вычисления
+	* @value поле результат вычисления
 	*/
 	private double result;
 	/**
-
-		Арифметические операции. @param params Аргументы операций.
+	* Сложение
+	* @param params аргументы сложения
 	*/
 	public void summ(double ... params) {
 		for (double param : params) {
 		this.result += param;
 		}
 	}
-
+	/**
+	* Умножение
+	* @param double a, double b аргументы умножения
+	*/
 	public void mult(double a, double b) {
 		this.result = a*b;
 	}
+	/**
+	* Вычитание
+	* @param double a, double b аргументы вычитания
+	*/
 	public void sub(double a, double b) {
 		this.result = a-b;
 	}
+	/**
+	* Деление
+	* @param double a, double b аргументы деления
+	*/
 	public void div(double a, double b){
 		this.result = a/b;
 	}
+	/**
+	* Возведение в степень
+	* @param double a основание, double b показатель степени
+	*/
 	public void pow(double a, double b) {
 		this.result = Math.pow (a, b);
 	}
  	/**
-		Получить результат. @result - результат вычисления.
+	* Получить результат. @result - результат вычисления
 	*/
 	public double getResult() {
 		return this.result;
 	}
-
-
 	/**
-		Очистиь результат вычислений
+	* Очистиь результат вычислений
 	*/
 	public void cleanResult() {
 		this.result = 0;
